484d85f3e328c59b9f5eb410d7e050190a5f982b
Furkan Tombaş <tombasfurkan@gmail.com>
2024-05-23 23:44:37 +0300

    Login page mobile view fixed

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function Page() {
 	const [room, setRoom] = useState("");
 
 	return (
-		<div className="w-1/4 h-screen flex flex-col gap-4 items-center justify-center mx-auto">
-			<div className="grid grid-cols-2 gap-4 w-full">
+		<div className="md:w-[90%] lg:w-1/2 2xl:w-1/3 h-screen flex flex-col gap-4 items-center justify-center sm:mx-6 mx-4 md:mx-auto">
+			<div className="grid sm:grid-cols-2 gap-4 w-full">
 				<input type="text" placeholder="Username" className="py-3 px-6 w-full z-20 text-md text-white rounded-full border-2 bg-gray-700  border-gray-600 placeholder-gray-400 focus:border-indigo-600 outline-none" value={user.userName} onChange={(e) => setUsername(e.target.value)} />
 				<input type="text" placeholder="Room" className="py-3 px-6 w-full z-20 text-md text-white rounded-full border-2 bg-gray-700  border-gray-600 placeholder-gray-400 focus:border-indigo-600 outline-none" value={room} onChange={(e) => setRoom(e.target.value)} />
 			</div>
-			<Link href={`/room/${room}`} className="py-3 px-6 w-full z-20 text-md text-white rounded-full border-2 bg-gray-700  border-gray-600 placeholder-gray-400 hover:border-indigo-600 hover:bg-indigo-600 outline-none transition-all">
+			<Link href={`/room/${room}`} className={`text-center py-3 px-6 w-full z-20 text-md text-white rounded-full border-2 bg-gray-700  border-gray-600 placeholder-gray-400 outline-none transition-all ${!user.userName || !room ? "cursor-not-allowed" : "hover:border-indigo-600 hover:bg-indigo-600"}`} onClick={(e) => (!user.userName || !room) && e.preventDefault()}>
 				Join Chat
 			</Link>
 		</div>
